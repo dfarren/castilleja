0a3e033bbc4a7f9019ed8b7f56042446f54ef15e
Derek <derek@walmartlabs.com>
2016-10-11 01:03:39 -0700

    final

--- regression/toolkit.py
+++ regression/toolkit.py
@@ -1,6 +1,5 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import ticker
+import numpy as np
 from sklearn.datasets import fetch_olivetti_faces
 from sklearn.utils.validation import check_random_state
 from sklearn.linear_model import SGDRegressor, LinearRegression
@@ -12,33 +11,61 @@ def generate_linear_data(slope, intercept, noise_std, max_x=100):
     x = np.arange(max_x)
     x = np.reshape(x, (max_x,1))
     y = np.array([i*slope+intercept+np.random.normal(0, noise_std) for i in x])
-    y = np.reshape(y, (y.shape[0],1))
-    return x, y
+    return x, np.ravel(y)
 
 
 def generate_quadratic_data(a, b, c, noise_std, min_x=0, max_x=100):
     x = np.reshape(np.arange(max_x), (max_x,1))
+    x = np.reshape(x, (max_x,1))
     y = np.array([a*i**2+b*i+c+np.random.normal(0, noise_std) for i in x])
-    return x, y
+    return x, np.ravel(y)
 
 
-def generate_unscaled_data(coefs, intercept, noise_std, min_x=0, max_x=100, scale=1000):
-    x = np.array([[e, e*scale] for e in np.arange(max_x)])
-    y = np.array([x1*coefs[0]+x2*coefs[1]+intercept+np.random.normal(0, noise_std) for x1, x2 in x])
-    return x, y
+def generate_unscaled_data(coefs, intercept, noise_std, min_x=-100, max_x=100, scale=100):
+    x1 = np.array([[e, 0] for e in np.arange(min_x, max_x)])
+    x2 = np.array([[0, e*scale] for e in np.arange(min_x, max_x)])
+    x = np.vstack((x1, x2))
+    y = np.array([x1**2*coefs[0]+x2**2*coefs[1]+intercept+np.random.normal(0, noise_std) for x1, x2 in x])
+    pdb.set_trace()
+    return x, np.ravel(y)
 
 
 def plot(x, y, predictions=None, x_label='x', y_label='y', line=False, line_weight=4):
-    fig, ax = plt.subplots()
-    if line:
-        ax.plot(x,y, lw=line_weight)
-    else:
-        ax.scatter(x, y)
-    if isinstance(predictions, np.ndarray):
-        ax.plot(x, predictions, 'r-', lw=line_weight)
-    ax.set_xlabel(x_label)
-    ax.set_ylabel(y_label)
-    plt.show()
+    # 2D plot
+    if len(x.shape)==1 or x.shape[1]==1:
+        fig, ax = plt.subplots()
+        if line:
+            ax.plot(x,y, lw=line_weight)
+        else:
+            ax.scatter(x, y)
+        if isinstance(predictions, np.ndarray):
+            ax.plot(x, predictions, 'r-', lw=line_weight)
+        ax.set_xlabel(x_label)
+        ax.set_ylabel(y_label)
+        plt.show()
+
+    # 3D plot
+    elif x.shape[1]==2:
+        from mpl_toolkits.mplot3d import Axes3D
+        from matplotlib import cm
+        from matplotlib.ticker import LinearLocator, FormatStrFormatter
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        X = x[:, 0]
+        Y = x[:, 1]
+        X, Y = np.meshgrid(X, Y)
+        Z = y
+        surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
+                               linewidth=0, antialiased=False)
+        #ax.set_zlim(-1.01, 1.01)
+
+        ax.zaxis.set_major_locator(LinearLocator(10))
+        ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+        fig.colorbar(surf, shrink=0.5, aspect=5)
+
+        plt.show()
+
     return
 
 
@@ -152,6 +179,6 @@ def plot_faces(X_test, y_test, y_test_predict):
     plt.show()
 
 if __name__=='__main__':
-    print huber(np.array([1,2,3]), 2)
 
-
+    x, y = generate_unscaled_data(coefs=[1,1], intercept=1, noise_std=10, scale=10)
+    plot(x, y)
